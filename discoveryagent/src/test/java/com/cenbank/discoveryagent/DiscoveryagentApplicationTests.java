5d2e73b399057059478fa728ffb43709d63310d7
dvcsr <4th3sh0ws@gmail.com>
2025-04-10 15:39:42 +0700

    init discoveryagent spring boot app

---
+++ discoveryagent/src/test/java/com/cenbank/discoveryagent/DiscoveryagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenbank.discoveryagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryagentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
