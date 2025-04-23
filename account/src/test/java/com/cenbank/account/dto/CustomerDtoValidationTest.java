968ecdf44a03b1a044eb6de74d1fc8781a794d98
dvcsr <4th3sh0ws@gmail.com>
2025-04-23 12:37:01 +0700

    log conflict with GraalVM. fixed: remove GraalVM and log is running

--- account/src/test/java/com/cenbank/account/dto/CustomerDtoValidationTest.java
+++ account/src/test/java/com/cenbank/account/dto/CustomerDtoValidationTest.java
@@ -6,20 +6,26 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CustomerDtoValidationTest {
-private static Validator validator;
+    private static final Logger logger = LoggerFactory.getLogger(CustomerDtoValidationTest.class);
+    private static Validator validator;
+
     @BeforeAll
     public static void setUpValidator() {
+        logger.debug("initializing validator for customerDto tests");
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
 
     @Test
     public void testCustomerDtoValidation_NoViolations() {
+        logger.info("Testing valid customer scenario");
         CustomerDto customer = new CustomerDto();
         customer.setCountryId("ID");
         customer.setPhoneNumber("628123456789");
@@ -29,10 +35,12 @@ private static Validator validator;
 
         var violations = validator.validate(customer);
         assertTrue(violations.isEmpty());
+        logger.info("Validation passed successfully");
     }
 
     @Test
     public void whenInvalidPhoneForCountry_thenViolation() {
+        logger.info("Testing invalid phone to country scenario");
         CustomerDto customer = new CustomerDto();
         customer.setCountryId("US");
         customer.setPhoneNumber("628123456789"); // Indonesian number with US country
@@ -45,12 +53,12 @@ private static Validator validator;
         assertEquals(1, violations.size(), "Should have exactly one violation!");
 
         ConstraintViolation<CustomerDto> violation = violations.iterator().next();
-        System.out.println("Validation failed: " + violation.getMessage());
-        assertEquals("Phone number doesn't match the country code", violation.getMessage());
+        logger.warn("Validation failed: {}", violation.getMessage());
     }
 
     @Test
     public void whenInvalidCountryID_thenViolation() {
+        logger.info("Testing invalid country Id scenario");
         CustomerDto customer = new CustomerDto();
         customer.setCountryId("NZ");
         customer.setPhoneNumber("628123456789"); // Indonesian number with US country
@@ -62,6 +70,6 @@ private static Validator validator;
         assertFalse(violations.isEmpty());
 
         ConstraintViolation<CustomerDto> violation = violations.iterator().next();
-        System.out.println("Validation failed: " + violation.getMessage());
+        logger.warn("Validation failed: {}", violation.getMessage());
     }
 }
